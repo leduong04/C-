09cbac24ce853cda0fff8c9fb80ca44f711f0a27
leduong04 <119992125+leduong04@users.noreply.github.com>
2023-01-27 21:29:27 +0700

    Tìm thừa sô nguyên tố lớn nhất của một số

---
+++ UOCNGUYENTOMAX.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int abc(int i)
+{
+    int ktra=1;
+    for(int j=2; j<i; j++)
+    {
+        if(i%j==0)
+        ktra=0;
+    }
+    return ktra;
+}
+
+int main()
+{
+    int n;
+    int c=0;
+    int d=0;
+
+    cout <<"N = ";
+    cin >> n;
+	
+    for(int i = 2; i<n; i++)
+    {
+        if(n%i==0)
+        {
+        	d=1;
+            if(abc(i)==1)
+            c=i;
+        }
+
+    }
+    if(d==0)
+    cout<<"Thua so nguyen to lon nhat cua "<<n<<" la: "<<n;
+
+    if(c!=0)
+	cout<<"Thua so nguyen to lon nhat cua "<<n<<" la: "<<c;
+
+}
+
+
